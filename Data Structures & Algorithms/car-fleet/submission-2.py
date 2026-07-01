ce4819f3e106c8124f1dadd1b1022624babdc8d8
Palatip Boonmeerit <palatip.boonmeerit@student.unimelb.edu.au>
2026-07-01 20:58:16 +1000

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pos_speed = {}
+
+        for i in range (0,len(position)):
+            pos_speed[position[i]] = speed[i]
+        tuple_sorted = sorted(pos_speed.items() , key=lambda x: x[0])
+
+        stack = []
+        for i in range (len(tuple_sorted) - 1 , -1, -1):
+            arrivalTimeCurr = (target - tuple_sorted[i][0]) / tuple_sorted[i][1]
+            if not stack or arrivalTimeCurr > stack[-1]:
+                stack.append(arrivalTimeCurr)
+        return len(stack)
