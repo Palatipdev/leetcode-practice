c61cb05139692297b70129ba0b1694b49c6adecf
Palatip Boonmeerit <palatip.boonmeerit@student.unimelb.edu.au>
2026-09-07 04:36:00 +1000

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        # using while loop
+        # if a single / found, continue until a character is found
+        # if a single . found, concacenate to check the length
+        # if a char is found, concacinate until / . is found
+        # should take single pass, O(n)
+        # sidenote: above is the bruteforce but this feels like a sliding window question
+
+        output = ""
+
+        splitPath = path.split("/")
+        stack = []
+       
+        for block in splitPath:
+            if block == "" or block == ".":
+                continue
+            elif block == ".." and stack:
+                stack.pop(-1)
+            elif block == "..":
+                continue
+            else:
+                stack.append(block)
+
+        if not stack:
+            return "/"
+
+        for block in stack:
+            output += "/" + block
+
+        return output
+        
